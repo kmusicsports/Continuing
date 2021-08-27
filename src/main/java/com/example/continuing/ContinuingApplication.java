404c12af26f994d88cad42860e08b931f73eb988
kmusicsports <naokik.musicsports1027@gmail.com>
2021-08-27 15:14:42 +0900

    first commit

---
+++ src/main/java/com/example/continuing/ContinuingApplication.java
@@ -0,0 +1,13 @@
+package com.example.continuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/continuing/ContinuingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.continuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
