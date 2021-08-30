a601885b1919da003fbfdc1bd0b8f04a67aba4b0
kmusicsports <naokik.musicsports1027@gmail.com>
2021-08-30 20:36:27 +0900

    add Users.java

---
+++ src/main/java/com/example/continuing/entity/Users.java
@@ -0,0 +1,51 @@
+package com.example.continuing.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "profile_image")
+	private String profile_image;
+	
+	@Column(name = "profile_message")
+	private String profile_message;
+	
+	@Column(name = "continuous_days")
+	private Integer continuous_days;
+	
+	@Column(name = "created_at")
+	private Timestamp created_at;
+	
+	@Column(name = "updated_at")
+	private Timestamp updated_at;
+}
