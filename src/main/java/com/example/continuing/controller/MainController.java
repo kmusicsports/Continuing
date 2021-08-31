60d6301ff0ee685d94349ef28dc5e7d87311b9b9
kmusicsports <naokik.musicsports1027@gmail.com>
2021-08-31 14:15:13 +0900

    fix MainController.java

--- src/main/java/com/example/continuing/controller/MainController.java
+++ src/main/java/com/example/continuing/controller/MainController.java
@@ -1,15 +1,28 @@
 package com.example.continuing.controller;
 
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.continuing.entity.Users;
+import com.example.continuing.repository.UsersRepository;
+
+import lombok.AllArgsConstructor;
+
 @Controller
+@AllArgsConstructor
 public class MainController {
+
+	private final UsersRepository usersRepository;
 	
-	@GetMapping("/Meeting/list/all")
+	@GetMapping("/Meeting/list/all") // "/home"
 	public ModelAndView showHome(ModelAndView mv) {
+		List<Users> userList = usersRepository.findAll();
 		mv.setViewName("home");
+		mv.addObject("userList", userList);
 		return mv;
 	}
 }
