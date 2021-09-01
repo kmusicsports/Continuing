a869c9a6bd68217c442da687026808dca7a5942b
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-01 14:44:13 +0900

    add check to LoginService.java

--- src/main/java/com/example/continuing/service/LoginService.java
+++ src/main/java/com/example/continuing/service/LoginService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.continuing.common.Utils;
 import com.example.continuing.entity.Users;
 import com.example.continuing.form.RegisterData;
 import com.example.continuing.repository.UsersRepository;
@@ -19,11 +20,11 @@ public class LoginService {
 
 	// 登録画面用のチェック
 	public boolean isValid(RegisterData registerData) {
-		Boolean flag = true;
+		Boolean answer = true;
 		
 		if(!registerData.isChecked()) {
 			System.out.println("Error: 利用規約に同意して、チェックを入れてください");
-			flag = false;
+			answer = false;
 		}
 		
 		if(!registerData.getPassword().equals(registerData.getPasswordAgain())) {
@@ -31,26 +32,35 @@ public class LoginService {
 			System.out.println("Error: パスワードが一致しません");
 			registerData.setPassword(null);
 			registerData.setPasswordAgain(null);
-			flag = false;
+			answer = false;
 		}
 		
 		Optional<Users> someUser;
-		someUser = usersRepository.findByName(registerData.getName());
+		String name = registerData.getName(); 
+		someUser = usersRepository.findByName(name);
 		if(someUser.isPresent()) {
 			// 既に同じ名前が登録されている ->　別の名前で登録してください
 			System.out.println("Error: 既に登録されている名前です");
 			registerData.setName(null);
-			flag = false;
+			answer = false;
 		}
+		
+		// 名前が全角スペースだけで構成されていたらエラー
+		if (!Utils.isBlank(name)) {
+			if (Utils.isAllDoubleSpace(name)) {
+				answer = false;
+			}
+		}
+		
 		someUser = usersRepository.findByEmail(registerData.getEmail());		
 		if(someUser.isPresent()) {
 			// 既にemailアドレスが登録されている ->　別のemailアドレスで登録してください
 			System.out.println("Error: 既に登録されているメールアドレスです");
 			registerData.setEmail(null);
-			flag = false;
+			answer = false;
 		}
 		
-		return flag;
+		return answer;
 	}
 
 }
