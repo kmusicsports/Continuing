a6ec2db8d4cd58ab62c35de3f0bc827efbddfd30
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-01 15:09:10 +0900

    add uploadProfileImage-method to UserController.java

--- src/main/java/com/example/continuing/controller/UserController.java
+++ src/main/java/com/example/continuing/controller/UserController.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.continuing.entity.Users;
 import com.example.continuing.form.ProfileData;
 import com.example.continuing.repository.UsersRepository;
+import com.example.continuing.service.StorageService;
 import com.example.continuing.service.UserService;
 
 import lombok.AllArgsConstructor;
@@ -24,6 +27,7 @@ public class UserController {
 	private final UsersRepository usersRepository;
 	private final HttpSession session;
 	private final UserService userService;
+	private final StorageService storageService;
 	
 	@GetMapping("/User/{id}")
 	public ModelAndView showUserDetail(ModelAndView mv, @PathVariable(name = "id") int id) {
@@ -97,5 +101,26 @@ public class UserController {
 			}			
 		}
 	}
+	
+	@PostMapping("/User/profileImage/upload")
+	public String uploadProfileImage(@RequestParam(value = "file") MultipartFile file) {
+		Integer id = (Integer)session.getAttribute("user_id");
+		if(id == null) {
+			System.out.println("Error: ログインし直してください");
+			return "redirect:/User/showLogin";
+		} else {
+			Users user = usersRepository.findById(id).get();
+			// 既にプロフィール画像があれば、それをS3から削除する
+			String oldProfileImage = user.getProfileImage();
+			if(oldProfileImage != null) {
+				storageService.deleteFile(oldProfileImage);
+			}
+			// アップロード & 変更
+			String profile_image = storageService.uploadFile(file);
+			user.setProfileImage(profile_image);
+			usersRepository.saveAndFlush(user);
+			return "redirect:/User/updateForm";			
+		}
+    }
 	
 }
