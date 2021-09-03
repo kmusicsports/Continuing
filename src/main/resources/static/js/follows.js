5c94b18ff28cee382581caac0acc85c11aacaa0a
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-03 16:56:19 +0900

    add follows.js

---
+++ src/main/resources/static/js/follows.js
@@ -0,0 +1,41 @@
+const followsTab = document.getElementById("pills-follows-tab");
+const followersTab = document.getElementById("pills-followers-tab");
+const followsTabContent = document.getElementById("pills-follows");
+const followersTabContent = document.getElementById("pills-followers");
+
+if (sessionStorage.getItem("tab") == "followers") {
+    followersTab.classList.add("active");
+    followersTabContent.classList.add("show")
+    followersTabContent.classList.add("active")
+    followsTab.classList.remove("active");
+    followsTabContent.classList.remove("show")
+    followsTabContent.classList.remove("active")
+} else {
+    followsTab.classList.add("active");
+    followsTabContent.classList.add("show")
+    followsTabContent.classList.add("active")
+    followersTab.classList.remove("active");
+    followersTabContent.classList.remove("show")
+    followersTabContent.classList.remove("active")
+}
+
+followsTab.addEventListener("click", function () {
+    sessionStorage.setItem("tab", "follows");
+});
+
+followersTab.addEventListener("click", function () {
+    sessionStorage.setItem("tab", "followers");
+});
+
+
+$(window).scroll(function () {
+  //set scroll position in session storage
+  sessionStorage.scrollPos = $(window).scrollTop();
+});
+
+var init = function () {
+  //return scroll position in session storage
+  $(window).scrollTop(sessionStorage.scrollPos || 0);
+};
+
+window.onload = init;
