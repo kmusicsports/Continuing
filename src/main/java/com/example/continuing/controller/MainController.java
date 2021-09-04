894f57d8fe1b2831492e9b2d7a653ade5887483d
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-04 12:39:53 +0900

    add topicList to showHome-method in MainController.java

--- src/main/java/com/example/continuing/controller/MainController.java
+++ src/main/java/com/example/continuing/controller/MainController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.continuing.entity.Topics;
 import com.example.continuing.entity.Users;
+import com.example.continuing.repository.TopicsRepository;
 import com.example.continuing.repository.UsersRepository;
 import com.example.continuing.service.FollowService;
 
@@ -22,15 +24,21 @@ public class MainController {
 	private final UsersRepository usersRepository;
 	private final HttpSession session;
 	private final FollowService followService;
+	private final TopicsRepository topicsRepository;
 	
 	@GetMapping("/Meeting/list/all") // "/"
 	public ModelAndView showHome(ModelAndView mv) {
 		List<Users> userList = usersRepository.findAll();
+		
 		Integer userId = (Integer)session.getAttribute("user_id");
 		List<Users> followsList = followService.getFollowsList(userId);
+		
+		List<Topics> topicList = topicsRepository.findAll();
+		
 		mv.setViewName("home");
 		mv.addObject("userList", userList);
 		mv.addObject("followsList", followsList);
+		mv.addObject("topicList", topicList);
 		return mv;
 	}
 }
