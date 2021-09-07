65fce83ff9a2a752e4bc815996b6b20786dfcff5
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-07 15:56:16 +0900

    add date and time validation to MeetingService.java

--- src/main/java/com/example/continuing/service/MeetingService.java
+++ src/main/java/com/example/continuing/service/MeetingService.java
@@ -1,5 +1,8 @@
 package com.example.continuing.service;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 import com.example.continuing.common.Utils;
@@ -26,14 +29,42 @@ public class MeetingService {
 			answer = false;
 		}
 		
-		int duration = Utils.stringToInt(meetingData.getEndTime()) - Utils.stringToInt(meetingData.getStartTime());
+		int duration = Utils.string2Int(meetingData.getEndTime()) - Utils.string2Int(meetingData.getStartTime());
 		if(meetingData.getNumberPeople() == 2 && duration > 40) {
-			System.out.println("Error: 複数人でのミーティングは最大40分です");
+			System.out.println("Error: 複数人でのミーティング時間は最大40分です");
+			meetingData.setStartTime(null);
+			meetingData.setEndTime(null);
+			answer = false;
+		} else if(meetingData.getNumberPeople() == 1 && duration > 1800) {
+			System.out.println("Error: ミーティング時間は最大30時間(1800分)です");
 			meetingData.setStartTime(null);
 			meetingData.setEndTime(null);
 			answer = false;
 		}
 		
-		return answer;
+		String date = meetingData.getDate().replace("/", "-");
+		LocalDate today = LocalDate.now();
+        LocalDate localeDate = null;
+        try {
+            // parseできればyyyy-mm-dd形式とみなす
+        	localeDate = LocalDate.parse(date);
+            if (localeDate.isBefore(today)) {
+                // 過去日付なのでfalse
+            	System.out.println("今日以降の日付を入力してください");
+                answer =  false;
+            }
+        } catch (DateTimeException e) {
+            // yyyy-mm-dd形式以外
+        	System.out.println("Error: 日付はyyyy-mm-dd または　yyyy/mm/dd　の形式で入力してください");
+        	e.printStackTrace();
+            answer =  false;
+        }
+        
+        if (!Utils.checkTimeFormat(meetingData.getEndTime()) || !Utils.checkTimeFormat(meetingData.getEndTime())) {
+        	System.out.println("Error: 時間はHH:mm　の形式で入力してください");
+        	answer = false;
+        }
+        
+        return answer;
 	}
 }
