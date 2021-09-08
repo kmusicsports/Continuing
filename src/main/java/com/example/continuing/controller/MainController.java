3c0e8fa6e7c45447c98d793b48859f0defef2203
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-08 15:46:03 +0900

    add meetingPage to showHome-method in MainController.java

--- src/main/java/com/example/continuing/controller/MainController.java
+++ src/main/java/com/example/continuing/controller/MainController.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.continuing.entity.Meetings;
 import com.example.continuing.entity.Topics;
 import com.example.continuing.entity.Users;
+import com.example.continuing.repository.MeetingsRepository;
 import com.example.continuing.repository.TopicsRepository;
 import com.example.continuing.repository.UsersRepository;
 import com.example.continuing.service.FollowService;
@@ -25,6 +30,7 @@ public class MainController {
 	private final HttpSession session;
 	private final FollowService followService;
 	private final TopicsRepository topicsRepository;
+	private final MeetingsRepository meetingsRepository;
 	
 	@GetMapping("/")
 	public String showHome() {
@@ -32,7 +38,9 @@ public class MainController {
 	}
 	
 	@GetMapping("/home")
-	public ModelAndView showHome(ModelAndView mv) {
+	public ModelAndView showHome(ModelAndView mv, 
+			@PageableDefault(page = 0, size = 10, sort = "id") Pageable pageable) {
+		Page<Meetings> meetingPage = meetingsRepository.findAll(pageable);
 		List<Users> userList = usersRepository.findAll();
 		
 		Integer userId = (Integer)session.getAttribute("user_id");
@@ -41,6 +49,8 @@ public class MainController {
 		List<Topics> topicList = topicsRepository.findAll();
 		
 		mv.setViewName("home");
+		mv.addObject("meetingPage", meetingPage);
+		mv.addObject("meetingList", meetingPage.getContent());
 		mv.addObject("userList", userList);
 		mv.addObject("myFollowsList", myFollowsList);
 		mv.addObject("topicList", topicList);
