e4e87c81e8ba2ddfabc6c4477ce892c90b2b4630
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-08 15:38:26 +0900

    add findByHost-method to MeetingsRepository.java

--- src/main/java/com/example/continuing/repository/MeetingsRepository.java
+++ src/main/java/com/example/continuing/repository/MeetingsRepository.java
@@ -1,9 +1,13 @@
 package com.example.continuing.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.continuing.entity.Meetings;
+import com.example.continuing.entity.Users;
 
 public interface MeetingsRepository extends JpaRepository<Meetings, Integer> {
 
+	List<Meetings> findByHost(Users host);
 }
