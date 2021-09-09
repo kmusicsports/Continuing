7093f7c8b36c1b4e287db5babef2d43e84d298fa
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-09 19:59:56 +0900

    add main.js to follows.html

--- src/main/resources/static/js/follows.js
+++ src/main/resources/static/js/follows.js
@@ -26,16 +26,3 @@ followsTab.addEventListener("click", function () {
 followersTab.addEventListener("click", function () {
     sessionStorage.setItem("tab", "followers");
 });
-
-
-$(window).scroll(function () {
-  //set scroll position in session storage
-  sessionStorage.scrollPos = $(window).scrollTop();
-});
-
-var init = function () {
-  //return scroll position in session storage
-  $(window).scrollTop(sessionStorage.scrollPos || 0);
-};
-
-window.onload = init;
