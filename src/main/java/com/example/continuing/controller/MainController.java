b6b5c9c32a225004c956f2f94096feed3cf4c5f8
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-11 23:21:07 +0900

    add search-method and fix showHome-method in MainController.java

--- src/main/java/com/example/continuing/controller/MainController.java
+++ src/main/java/com/example/continuing/controller/MainController.java
@@ -3,6 +3,9 @@ package com.example.continuing.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.data.domain.Page;
@@ -10,29 +13,42 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.continuing.dao.MeetingsDaoImpl;
 import com.example.continuing.entity.Meetings;
 import com.example.continuing.entity.Topics;
 import com.example.continuing.entity.Users;
-import com.example.continuing.repository.MeetingsRepository;
+import com.example.continuing.form.SearchData;
 import com.example.continuing.repository.TopicsRepository;
 import com.example.continuing.repository.UsersRepository;
 import com.example.continuing.service.FollowService;
 import com.example.continuing.service.JoinService;
+import com.example.continuing.service.MeetingService;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MainController {
 
 	private final UsersRepository usersRepository;
 	private final HttpSession session;
 	private final FollowService followService;
 	private final TopicsRepository topicsRepository;
-	private final MeetingsRepository meetingsRepository;
 	private final JoinService joinService;
+	private final MeetingService meetingService;
+	
+	@PersistenceContext
+    private EntityManager entityManager;
+	private MeetingsDaoImpl meetingsDaoImpl;
+	
+	@PostConstruct
+    public void init() {
+		meetingsDaoImpl = new MeetingsDaoImpl(entityManager);
+	}
+	
 	
 	@GetMapping("/")
 	public String showHome() {
@@ -42,7 +58,22 @@ public class MainController {
 	@GetMapping("/home")
 	public ModelAndView showHome(ModelAndView mv, 
 			@PageableDefault(page = 0, size = 10, sort = "id") Pageable pageable) {
-		Page<Meetings> meetingPage = meetingsRepository.findAll(pageable);
+		// sessionから前回の検索条件を取得
+		SearchData searchData = (SearchData)session.getAttribute("searchData");
+		if (searchData == null) {
+			searchData = new SearchData();
+			session.setAttribute("searchData", searchData);
+		}
+		
+		 // sessionから前回のpageableを取得
+        Pageable prevPageable = (Pageable)session.getAttribute("prevPageable");
+        if (prevPageable == null) {
+            // なければ@PageableDefaultを使う
+            prevPageable = pageable;
+            session.setAttribute("prevPageable", prevPageable);
+        }
+		
+		Page<Meetings> meetingPage = meetingsDaoImpl.findByCriteria(searchData, prevPageable);
 		List<Users> userList = usersRepository.findAll();
 		List<Topics> topicList = topicsRepository.findAll();
 		
@@ -58,6 +89,50 @@ public class MainController {
 		mv.addObject("topicList", topicList);
 		mv.addObject("myFollowsList", myFollowsList);
 		mv.addObject("myJoinMeetingList", myJoinMeetingList);
+		mv.addObject("searchData", searchData);
 		return mv;
 	}
+	
+	@PostMapping("/search")
+	public ModelAndView search(ModelAndView mv, SearchData searchData, 
+			@PageableDefault(page = 0, size = 10, sort = "id") Pageable pageable) {
+		List<Users> userList = usersRepository.findAll();
+		List<Topics> topicList = topicsRepository.findAll();
+		
+		Integer userId = (Integer)session.getAttribute("user_id");
+		List<Users> myFollowsList = followService.getFollowsList(userId);
+		List<Meetings> myJoinMeetingList = joinService.getJoinMeetingList(userId);
+		
+		session.setAttribute("path", "/home");
+		mv.setViewName("home");
+		
+		if (meetingService.isValid(searchData)) {			
+			Page<Meetings> meetingPage = meetingsDaoImpl.findByCriteria(searchData, pageable);
+			
+			// 入力された検索条件をsessionへ保存
+			session.setAttribute("searchData", searchData);
+			mv.addObject("meetingPage", meetingPage);
+			mv.addObject("meetingList", meetingPage.getContent());
+			
+			if (meetingPage.getContent().size() == 0) {
+				System.out.println("該当する検索結果はありません");
+				// 該当なかったらメッセージを表示
+				
+			}
+		} else {
+			System.out.println("入力に間違いがあります");
+			mv.addObject("meetingPage", null);
+			mv.addObject("meetingList", null);
+			// 検索条件エラーあり -> エラーメッセージをセット
+			
+		}
+		
+		mv.addObject("userList", userList);
+		mv.addObject("topicList", topicList);
+		mv.addObject("myFollowsList", myFollowsList);
+		mv.addObject("myJoinMeetingList", myJoinMeetingList);
+		mv.addObject("searchData", searchData);
+		return mv;
+	}
+	
 }
