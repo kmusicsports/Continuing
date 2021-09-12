7813551f287aa63a359098018ac97194506e9f87
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-12 16:24:47 +0900

    add methods to UsersRepository.java

--- src/main/java/com/example/continuing/repository/UsersRepository.java
+++ src/main/java/com/example/continuing/repository/UsersRepository.java
@@ -1,5 +1,6 @@
 package com.example.continuing.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,7 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
 
 	Optional<Users> findByName(String name);
 	Optional<Users> findByEmail(String email);
+	List<Users> findByNameContainingIgnoreCase(String name);
+	List<Users> findByProfileMessageContainingIgnoreCase(String profileMessage);
+	
 }
