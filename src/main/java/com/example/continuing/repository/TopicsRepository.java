7ad6256474de1393faec7664d6e6b65980c1a09b
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-16 11:07:06 +0900

    add Repository-annotation to all Repository-class

--- src/main/java/com/example/continuing/repository/TopicsRepository.java
+++ src/main/java/com/example/continuing/repository/TopicsRepository.java
@@ -1,9 +1,11 @@
 package com.example.continuing.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.continuing.entity.Topics;
 
+@Repository
 public interface TopicsRepository extends JpaRepository<Topics, Integer> {
 
 }
