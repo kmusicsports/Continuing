1284a5c8167d3602bea66080203d374ec9a6a04d
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-20 19:19:31 +0900

    add setFrom to sendMail-method in MailService.java

--- src/main/java/com/example/continuing/service/MailService.java
+++ src/main/java/com/example/continuing/service/MailService.java
@@ -1,19 +1,29 @@
 package com.example.continuing.service;
 
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MailService {
 
 	private final JavaMailSender sender;
 	
+	@Value("${spring.mail.username}")
+	private String FROM_ADDRESS;
+	
+	private final static String FROM_NAME = "Continuing";
+	private final static String ENCODE = "UTF-8";
+	
+	@Async
 	public boolean sendMail(String mailAddress, String subject, String text) {
 		
 		MimeMessage message = sender.createMimeMessage();
@@ -22,6 +32,8 @@ public class MailService {
 		try {
 			
 			helper = new MimeMessageHelper(message, true);
+			InternetAddress fromAddress = new InternetAddress(FROM_ADDRESS, FROM_NAME, ENCODE);
+			helper.setFrom(fromAddress);
 			helper.setTo(mailAddress);
 			helper.setSubject(subject);
 			helper.setText(text, true);
