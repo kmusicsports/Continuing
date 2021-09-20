db2dc498a68e79c6248dc2eafd87e7b2c518eb26
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-20 19:28:02 +0900

    add annotation validation to LoginData.java and RegisterData.java

--- src/main/java/com/example/continuing/form/RegisterData.java
+++ src/main/java/com/example/continuing/form/RegisterData.java
@@ -3,6 +3,13 @@ package com.example.continuing.form;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.continuing.entity.Users;
@@ -12,10 +19,25 @@ import lombok.Data;
 @Data
 public class RegisterData {
 
+	@NotBlank
 	private String name;
+	
+	@NotBlank
+	@Email
 	private String email;
+	
+	@NotBlank
+	@Length(min = 8, max = 16)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	private String password;
+	
+	@NotBlank
+	@Length(min = 8, max = 16)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	private String passwordAgain;
+	
+	@NotNull
+	@AssertTrue
 	private boolean checked;
 	
 	public Users toEntity(PasswordEncoder passwordEncoder) {
