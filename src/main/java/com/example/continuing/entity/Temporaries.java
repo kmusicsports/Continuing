f4862c62e9aa7bf7fbe28e2c2dc377f051921d84
kmusicsports <naokik.musicsports1027@gmail.com>
2021-09-30 11:48:02 +0900

    add Temporaries.java

---
+++ src/main/java/com/example/continuing/entity/Temporaries.java
@@ -0,0 +1,57 @@
+package com.example.continuing.entity;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "temporaries")
+@Data
+@NoArgsConstructor
+public class Temporaries {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "user_id")
+    private Integer userId;
+	
+	@Column(name = "name")
+    private String name;
+	
+	@Column(name = "email")
+    private String email;
+	
+	@Column(name = "password")
+    private String password;
+	
+	@Column(name = "token")
+    private String token;
+	
+	@Column(name = "created_at")
+    private Timestamp createdAt;
+	
+	public Temporaries(Users user, String token) {
+		this.userId = user.getId();
+		this.name = user.getName();
+		this.email = user.getEmail();
+		this.password = user.getPassword();
+		this.token = token;
+		
+		Date date = new Date();
+		Timestamp timestamp = new Timestamp(date.getTime());
+		this.createdAt = timestamp;
+	}
+	
+}
