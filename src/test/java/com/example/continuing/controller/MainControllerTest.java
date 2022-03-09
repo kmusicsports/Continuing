ca176c0720748c24dd66dc43f51bc558efb13082
kmusicsports <naokik.musicsports1027@gmail.com>
2022-03-09 15:26:02 +0900

    add MainControllerTest.java

---
+++ src/test/java/com/example/continuing/controller/MainControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.continuing.controller;
+
+import com.example.continuing.repository.UsersRepository;
+import com.example.continuing.service.FollowService;
+import com.example.continuing.service.JoinService;
+import com.example.continuing.service.MeetingService;
+import com.example.continuing.service.UserService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.MessageSource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class MainControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FollowService followService;
+
+    @MockBean
+    private JoinService joinService;
+
+    @MockBean
+    private MeetingService meetingService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UsersRepository usersRepository;
+
+    @MockBean
+    private MessageSource messageSource;
+
+    @Autowired
+    private MainController mainController;
+
+
+}
