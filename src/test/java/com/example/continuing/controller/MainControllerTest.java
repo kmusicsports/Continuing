2eb4b93c9cd955d90563d7ceed209ce02ab85c21
kmusicsports <naokik.musicsports1027@gmail.com>
2022-03-09 16:35:06 +0900

    add testShowPrivacyPolicy method to MainControllerTest.java

--- src/test/java/com/example/continuing/controller/MainControllerTest.java
+++ src/test/java/com/example/continuing/controller/MainControllerTest.java
@@ -6,8 +6,11 @@ import com.example.continuing.service.JoinService;
 import com.example.continuing.service.MeetingService;
 import com.example.continuing.service.UserService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +19,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Locale;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -51,6 +56,21 @@ class MainControllerTest {
     private MainController mainController;
 
 
+
+    @ParameterizedTest
+    @CsvSource({"'th', 'privacy/privacyPolicy'",
+            "'ja', 'privacy/privacyPolicy_ja'",
+            "'en', 'privacy/privacyPolicy_en'",
+    })
+    @DisplayName("[showPrivacyPolicyメソッドのテスト]")
+    public void testShowPrivacyPolicy(String language, String viewName) throws Exception {
+        Locale locale = new Locale(language);
+
+        mockMvc.perform(get("/privacy").locale(locale))
+                .andExpect(status().isOk())
+                .andExpect(view().name(viewName))
+                .andExpect(model().size(0));
+    }
 
     @Test
     @DisplayName("[showVerifyZoomメソッドのテスト]")
