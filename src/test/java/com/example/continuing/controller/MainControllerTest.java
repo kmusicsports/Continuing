0bbdb3de0570527e63c2471ebd6ea10286bedad1
kmusicsports <naokik.musicsports1027@gmail.com>
2022-03-15 10:00:28 +0900

    add NestedTestShowHomeMAV to MainControllerTest.java

--- src/test/java/com/example/continuing/controller/MainControllerTest.java
+++ src/test/java/com/example/continuing/controller/MainControllerTest.java
@@ -1,11 +1,15 @@
 package com.example.continuing.controller;
 
+import com.example.continuing.dao.MeetingsDaoImpl;
+import com.example.continuing.entity.Meetings;
+import com.example.continuing.entity.Users;
 import com.example.continuing.form.SearchData;
 import com.example.continuing.repository.UsersRepository;
 import com.example.continuing.service.FollowService;
 import com.example.continuing.service.JoinService;
 import com.example.continuing.service.MeetingService;
 import com.example.continuing.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,11 +22,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Locale;
+import java.util.*;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -53,11 +61,14 @@ class MainControllerTest {
     @MockBean
     private MessageSource messageSource;
 
+    @MockBean
+    private MeetingsDaoImpl meetingsDaoImpl;
+
     @Autowired
     private MainController mainController;
 
     @Nested
-    @DisplayName("[showHome(Model model)メソッドのテスト]")
+    @DisplayName("[showHome(Model)メソッドのテスト]")
     public class NestedTestShowHomeModel {
 
         @Test
@@ -80,6 +91,110 @@ class MainControllerTest {
         }
     }
 
+    @Nested
+    @DisplayName("[showHome(ModelAndView, Pageable)メソッドのテスト]")
+    public class NestedTestShowHomeMAV {
+
+        private static final String PATH = "/home";
+        private List<Users> userList;
+        private List<Users> userRanking;
+        private Map<Integer, Integer> rankingMap;
+        private final int testUserId = 1;
+        private final int continuousDays = 2;
+        private List<Users> myFollowsList;
+        private List<Meetings> myJoinMeetingList;
+        private SearchData searchData;
+        private Users testUser1;
+        private Users testUser2;
+        private Users testUser3;
+        private Meetings testMeeting;
+
+        @BeforeEach
+        public void init() {
+            userList = new ArrayList<>();
+            userRanking = new ArrayList<>();
+            rankingMap = new TreeMap<>();
+            myFollowsList = new ArrayList<>();
+            myJoinMeetingList = new ArrayList<>();
+            searchData = new SearchData();
+            testUser1 = new Users();
+            testUser2 = new Users();
+            testUser3 = new Users();
+            testMeeting = new Meetings();
+
+            testUser1.setId(1);
+            testUser2.setId(2);
+            testUser3.setId(3);
+            testUser2.setContinuousDays(continuousDays);
+            testMeeting.setId(1);
+            userList.add(testUser1);
+            userRanking.add(testUser2);
+            rankingMap.put(continuousDays, 1);
+            myFollowsList.add(testUser3);
+            myJoinMeetingList.add(testMeeting);
+
+            when(usersRepository.findTop3By()).thenReturn(userRanking);
+            when(userService.makeRankingMap(userRanking)).thenReturn(rankingMap);
+            when(followService.getFollowsList(testUserId)).thenReturn(myFollowsList);
+            when(joinService.getJoinMeetingList(testUserId)).thenReturn(myJoinMeetingList);
+        }
+
+        @Test
+        @DisplayName("searchData == null && prevPageable == null")
+        public void nullAndNull() throws Exception {
+            when(userService.getSearchResult(searchData)).thenReturn(userList);
+
+            mockMvc.perform(get(PATH).sessionAttr("user_id", testUserId))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("home"))
+                    .andExpect(request().sessionAttribute("path", PATH))
+                    .andExpect(model().attributeExists("meetingPage"))
+                    .andExpect(model().attributeExists("meetingList"))
+                    .andExpect(model().attribute("userList", userList))
+                    .andExpect(model().attribute("myFollowsList", myFollowsList))
+                    .andExpect(model().attribute("myJoinMeetingList", myJoinMeetingList))
+                    .andExpect(model().attribute("searchData", searchData))
+                    .andExpect(model().attribute("userRanking", userRanking))
+                    .andExpect(model().attribute("rankingMap", rankingMap));
+
+            verify(userService, times(1)).getSearchResult(searchData);
+            verify(usersRepository, times(1)).findTop3By();
+            verify(userService, times(1)).makeRankingMap(userRanking);
+            verify(followService, times(1)).getFollowsList(testUserId);
+            verify(joinService, times(1)).getJoinMeetingList(testUserId);
+        }
+
+        @Test
+        @DisplayName("searchData != null && prevPageable == null")
+        public void notNullAndNull() throws Exception {
+            searchData.setKeyword("keyword");
+
+            Map<String, Object> sessionAttributes = new HashMap<>();
+            sessionAttributes.put("user_id", testUserId);
+            sessionAttributes.put("searchData", searchData);
+
+            when(userService.getSearchResult(searchData)).thenReturn(userList);
+
+            mockMvc.perform(get(PATH).sessionAttrs(sessionAttributes))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("home"))
+                    .andExpect(request().sessionAttribute("path", PATH))
+                    .andExpect(model().attributeExists("meetingPage"))
+                    .andExpect(model().attributeExists("meetingList"))
+                    .andExpect(model().attribute("userList", userList))
+                    .andExpect(model().attribute("myFollowsList", myFollowsList))
+                    .andExpect(model().attribute("myJoinMeetingList", myJoinMeetingList))
+                    .andExpect(model().attribute("searchData", searchData))
+                    .andExpect(model().attribute("userRanking", userRanking))
+                    .andExpect(model().attribute("rankingMap", rankingMap));
+
+            verify(userService, times(1)).getSearchResult(searchData);
+            verify(usersRepository, times(1)).findTop3By();
+            verify(userService, times(1)).makeRankingMap(userRanking);
+            verify(followService, times(1)).getFollowsList(testUserId);
+            verify(joinService, times(1)).getJoinMeetingList(testUserId);
+        }
+    }
 
 
     @ParameterizedTest
