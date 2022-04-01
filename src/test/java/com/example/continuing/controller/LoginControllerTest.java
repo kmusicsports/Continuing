7e56629e76fbf3f8c8984320438aa2b92d25683e
kmusicsports <naokik.musicsports1027@gmail.com>
2022-04-01 10:54:10 +0900

    add NestedTestShowMeetingDetail to MainMeetingControllerTest.java

--- src/test/java/com/example/continuing/controller/LoginControllerTest.java
+++ src/test/java/com/example/continuing/controller/LoginControllerTest.java
@@ -40,7 +40,6 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
--- src/test/java/com/example/continuing/controller/meeting/MainMeetingControllerTest.java
+++ src/test/java/com/example/continuing/controller/meeting/MainMeetingControllerTest.java
@@ -1,5 +1,8 @@
 package com.example.continuing.controller.meeting;
 
+import com.example.continuing.entity.Meetings;
+import com.example.continuing.entity.Users;
+import com.example.continuing.form.SearchData;
 import com.example.continuing.repository.JoinsRepository;
 import com.example.continuing.repository.MeetingsRepository;
 import com.example.continuing.repository.UsersRepository;
@@ -7,6 +10,10 @@ import com.example.continuing.service.FollowService;
 import com.example.continuing.service.JoinService;
 import com.example.continuing.service.MeetingService;
 import com.example.continuing.zoom.ZoomApiIntegration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -19,7 +26,16 @@ import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -60,5 +76,75 @@ class MainMeetingControllerTest {
     @Captor
     ArgumentCaptor<Locale> localeCaptor;
 
+    @Nested
+    @DisplayName("[showMeetingDetailメソッドのテスト]")
+    public class NestedTestShowMeetingDetail {
+
+        private final int testMeetingId = 1;
+        private final int testUserId = 2;
+        private final String path = "/Meeting/" + testMeetingId;
+        private Users testUser;
+
+        @BeforeEach
+        public void init() {
+            testUser = new Users();
+            testUser.setId(testUserId);
+        }
+
+        @Test
+        @DisplayName("指定したミーティングが見つからない場合")
+        public void meetingNotFound() throws Exception {
+            testUser.setLanguage("ja");
+
+            when(usersRepository.findById(testUserId)).thenReturn(Optional.of(testUser));
+
+            mockMvc.perform(get(path).sessionAttr("user_id", testUserId))
+                    .andExpect(status().isFound())
+                    .andExpect(redirectedUrl("/home"))
+                    .andExpect(flash().attributeExists("msg"));
+
+            verify(messageSource, times(1)).getMessage(any(), any(), localeCaptor.capture());
+            Locale capturedLocale = localeCaptor.getValue();
+            assertThat(capturedLocale).isEqualTo(new Locale(testUser.getLanguage()));
+        }
+
+        @Test
+        @DisplayName("指定したミーティングが見つかった場合")
+        public void meetingFound() throws Exception {
+
+            Meetings testMeeting = new Meetings();
+            testMeeting.setId(testMeetingId);
+            testMeeting.setHost(testUser);
+
+            Users testUser3 = new Users();
+            Users testUser4 = new Users();
+            testUser3.setId(3);
+            testUser4.setId(4);
+
+            List<Meetings> myJoinMeetingList = new ArrayList<>();
+            List<Users> myFollowsList = new ArrayList<>();
+            List<Users> joinUserList = new ArrayList<>();
+
+            myJoinMeetingList.add(testMeeting);
+            myFollowsList.add(testUser3);
+            joinUserList.add(testUser4);
+
+            when(joinService.getJoinMeetingList(testUserId)).thenReturn(myJoinMeetingList);
+            when(followService.getFollowsList(testUserId)).thenReturn(myFollowsList);
+            when(joinService.getJoinUserList(testMeeting)).thenReturn(joinUserList);
+
+            when(meetingsRepository.findById(testMeetingId)).thenReturn(Optional.of(testMeeting));
+
+            mockMvc.perform(get(path).sessionAttr("user_id", testUserId))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("meetingDetail"))
+                    .andExpect(request().sessionAttribute("path", path))
+                    .andExpect(model().attribute("meeting", testMeeting))
+                    .andExpect(model().attribute("myJoinMeetingList", myJoinMeetingList))
+                    .andExpect(model().attribute("joinUserList", joinUserList))
+                    .andExpect(model().attribute("myFollowsList", myFollowsList))
+                    .andExpect(model().attribute("searchData", new SearchData()));
+        }
+    }
 
 }
