9af23264e62d0f20d82a36d0c72bae51fe23f21f
kmusicsports <naokik.musicsports1027@gmail.com>
2022-04-11 08:47:15 +0900

    add NestedTestIsBlank to UtilsTest.java

--- src/test/java/com/example/continuing/common/UtilsTest.java
+++ src/test/java/com/example/continuing/common/UtilsTest.java
@@ -3,6 +3,8 @@ package com.example.continuing.common;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -20,17 +22,10 @@ class UtilsTest {
             assertTrue(actual);
         }
 
-        @Test
-        @DisplayName("空文字を渡す")
-        public void passEmptyChar() {
-            boolean actual = Utils.isAllDoubleSpace("");
-            assertTrue(actual);
-        }
-
-        @Test
-        @DisplayName("全角スペースを渡す")
-        public void passAllDoubleSpace() {
-            boolean actual = Utils.isAllDoubleSpace("　");
+        @ParameterizedTest
+        @CsvSource({"''", "'　'"})
+        public void tests(String s) {
+            boolean actual = Utils.isAllDoubleSpace(s);
             assertTrue(actual);
         }
 
@@ -38,6 +33,32 @@ class UtilsTest {
         @DisplayName("文字列を渡す")
         public void passNormalStr() {
             boolean actual = Utils.isAllDoubleSpace("test");
+            assertFalse(actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("[isBlankメソッドのテスト]")
+    public class NestedTestIsBlank {
+
+        @Test
+        @DisplayName("nullを渡す")
+        public void passNull() {
+            boolean actual = Utils.isBlank(null);
+            assertTrue(actual);
+        }
+
+        @ParameterizedTest
+        @CsvSource({"''", "' '", "'\t'"})
+        public void tests(String s) {
+            boolean actual = Utils.isBlank(s);
+            assertTrue(actual);
+        }
+
+        @Test
+        @DisplayName("文字列を渡す")
+        public void passNormalStr() {
+            boolean actual = Utils.isBlank("test");
             assertFalse(actual);
         }
     }
